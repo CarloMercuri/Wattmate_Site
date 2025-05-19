5d74717164e1f68393cf8943334d5f701ca347ae
cmerc <cmercuri85@gmail.com>
2025-05-19 23:15:27 +0200

    hh

---
+++ wwwroot/js/telemetry-charts.js
@@ -0,0 +1,45 @@
+﻿
+
+function initiateLineChart(_chartElement, _data) {
+    let myBarChart = new Chart(_chartElement, {
+        type: 'line',
+        data: _data,
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: '',
+                        font: {
+                            padding: 4,
+                            size: 20,
+                            weight: 'bold',
+                            family: 'Arial'
+                        },
+                        color: 'darkblue'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: '',
+                        font: {
+                            size: 20,
+                            weight: 'bold',
+                            family: 'Arial'
+                        },
+                        color: 'darkblue'
+                    },
+                    beginAtZero: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Values',
+                    }
+                }
+            }
+        }
+    });
+
+    return myBarChart;
+}
